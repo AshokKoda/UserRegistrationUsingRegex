342010de82499e6f14578438b6d48b5413d3d141
AshokKoda <a.koda59@gmail.com>
2022-04-08 20:16:53 +0530

    Added:Welcome message to user registration program

---
+++ src/main/java/com/bridgelabz/javaregex/UserRegistration.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.javaregex;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		System.out.println("*********Welcome to user registration with regex***********");
+
+	}
+
+}
