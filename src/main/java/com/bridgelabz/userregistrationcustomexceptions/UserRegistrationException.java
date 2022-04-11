f7655687e52ce3f1b8ff0442b9ad8309942515dd
AshokKoda <a.koda59@gmail.com>
2022-04-11 12:45:25 +0530

    Added:User Defined Exception for User Registration Problem

---
+++ src/main/java/com/bridgelabz/userregistrationcustomexceptions/UserRegistrationException.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.userregistrationcustomexceptions;
+
+public class UserRegistrationException extends Throwable {
+
+	public ExceptionType exceptionType;
+	public String message;
+
+	public enum ExceptionType {
+		EMPTY, NULL, INVALID;
+	}
+
+	public UserRegistrationException(ExceptionType exceptionType, String message) {
+		this.exceptionType = exceptionType;
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "UserRegistrationException{" + "exceptionType=" + exceptionType + ", message='" + message + '\'' + '}';
+	}
+
+}
